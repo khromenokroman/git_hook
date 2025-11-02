8184126a26381702893d83da1d5bcdc15e00c91c
khromenokroman <roma55592@yandex.ru>
2025-11-02 19:10:12 +0200

    feat: Добавлена установка хука
    Скрипт для проверки форматирования коммита добавлн к устаноке автомматически

--- scripts/git-hooks/setup_hooks.sh
+++ scripts/git-hooks/setup_hooks.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/bash
 
-HOOK_SRC="scripts/git-hooks/pre-commit"
-HOOK_DEST=".git/hooks/pre-commit"
+# Настройка pre-commit хука
+PRECOMMIT_SRC="scripts/git-hooks/pre-commit"
+PRECOMMIT_DEST=".git/hooks/pre-commit"
 
-if [[ ! -f "$HOOK_SRC" ]]; then
-   echo "Файл $HOOK_SRC не найден! Проверьте, что скрипт добавлен в проект."
+if [[ ! -f "$PRECOMMIT_SRC" ]]; then
+   echo "Файл $PRECOMMIT_SRC не найден! Проверьте, что скрипт добавлен в проект."
    exit 1
 fi
 
-cp "$HOOK_SRC" "$HOOK_DEST"
-chmod +x "$HOOK_DEST"
+cp "$PRECOMMIT_SRC" "$PRECOMMIT_DEST"
+chmod +x "$PRECOMMIT_DEST"
 echo "Git-хук pre-commit успешно настроен"
+
+# Настройка commit-msg хука
+COMMITMSG_SRC="scripts/git-hooks/commit-msg"
+COMMITMSG_DEST=".git/hooks/commit-msg"
+
+if [[ ! -f "$COMMITMSG_SRC" ]]; then
+   echo "Файл $COMMITMSG_SRC не найден! Проверьте, что скрипт добавлен в проект."
+   exit 1
+fi
+
+cp "$COMMITMSG_SRC" "$COMMITMSG_DEST"
+chmod +x "$COMMITMSG_DEST"
+echo "Git-хук commit-msg успешно настроен"
