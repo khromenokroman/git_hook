34e507cbe8830fa1cc38f7e6810e45486c6a555c
khromenokroman <roma55592@yandex.ru>
2025-11-02 19:36:24 +0200

    feat: Добавлена настройка pre-push хука

--- scripts/git-hooks/setup_hooks.sh
+++ scripts/git-hooks/setup_hooks.sh
@@ -25,3 +25,16 @@ fi
 cp "$COMMITMSG_SRC" "$COMMITMSG_DEST"
 chmod +x "$COMMITMSG_DEST"
 echo "Git-хук commit-msg успешно настроен"
+
+# Настройка pre-push хука
+PREPUSH_SRC="scripts/git-hooks/pre-push"
+PREPUSH_DEST=".git/hooks/pre-push"
+
+if [[ ! -f "$PREPUSH_SRC" ]]; then
+   echo "Файл $PREPUSH_SRC не найден! Проверьте, что скрипт добавлен в проект."
+   exit 1
+fi
+
+cp "$PREPUSH_SRC" "$PREPUSH_DEST"
+chmod +x "$PREPUSH_DEST"
+echo "Git-хук pre-push успешно настроен"
